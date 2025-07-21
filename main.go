42f930eb41092ac885513fba8bcd40519aff4696
sanu1001 <sanumukhopadhyay123@gmail.com>
2025-07-21 17:31:56 +0530

    final commit to age-bot

--- main.go
+++ main.go
@@ -1,10 +1,71 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/shomali11/slacker"
 )
 
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("command Events")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+	}
+}
+
 func main() {
-	fmt.Println("Hello, Slack Age Bot!")
-	// Additional code for the Slack bot will go here
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	fmt.Println("Hello,Welcome to Age Bot!")
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+	go printCommandEvents(bot.CommandEvents())
+
+	bot.Command("my yob is <year>", &slacker.CommandDefinition{
+		Description: "yob calculator",
+		Examples:    []string{"My yob is 1990"},
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+
+			year := request.Param("year")
+			yob, err := strconv.Atoi(year)
+			if err != nil {
+				response.Reply("Invalid year format. Please enter a valid year.")
+				return
+			}
+			currentYear := time.Now().Year()
+			age := currentYear - yob
+
+			response.Reply(fmt.Sprintf("Your age is %d years.", age))
+		},
+	})
+
+	bot.Command("very good age-bot!", &slacker.CommandDefinition{
+		Description: "Acknowledge the bot",
+		Examples:    []string{"Very good age-bot!"},
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			response.Reply("Thank you! I'm glad you think so!")
+		},
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err = bot.Listen(ctx)
+	if err != nil {
+		fmt.Println("Error starting the bot:", err)
+		log.Fatal(err)
+	}
+
 }
